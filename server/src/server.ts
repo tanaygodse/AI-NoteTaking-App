f4561f0c3462db65d9db84adc0b53fd1864e193d
Tanay Godse <tanaygodse@gmail.com>
2023-11-26 00:37:27 -0800

    Updates: Able to store notes now
    TODO: Fetch for frontend from DB, upload documents for this, testing images and whether all fields are always received or not, add post api call to frontend

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,25 +6,51 @@ import bodyParser from 'body-parser';
 import { ConnectionOptions } from 'tls';
 
 // Define types
-interface Content {
+interface BoundingRect {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  width: number;
+  height: number;
+  pageNumber: number;
+}
+
+interface Rect {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  width: number;
+  height: number;
+  pageNumber: number;
+}
+
+interface Position {
+  boundingRect: BoundingRect;
+  rects: Rect[];
+  pageNumber: number;
+}
+
+interface Comment {
+  text: string;
+  emoji: string;
+}
+
+interface ContentData {
   text: string;
   image: string;
 }
 
-interface PostDocument extends Document {
-  title: string;
-  content: Content;
+interface PostData {
+  content: ContentData;
+  position: Position;
+  comment: Comment;
 }
 
-const PostSchema = new Schema<PostDocument>({
-  title: { type: String, required: true },
-  content: {
-    text: { type: String, required: true },
-    image: { type: String, required: true },
-  },
-});
-
-const Post = mongoose.model<PostDocument>('Post', PostSchema);
+interface PostRequestData {
+  [url: string]: PostData;
+}
 
 const app = express();
 const port = 5001;
@@ -32,33 +58,87 @@ const port = 5001;
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/your-database-name', {
+mongoose.connect('mongodb://localhost:27017/notetaking-app', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 } as ConnectionOptions);
 
-app.get('/', (req: Request, res: Response) => {
-  console.log('Hello World!');
-})
+// Define Post model
+interface PostDocument extends Document {
+  title: string;
+  content: ContentData;
+  position: Position;
+  comment: Comment;
+}
+
+const PostSchema = new Schema<PostDocument>({
+  title: { type: String, required: true },
+  content: {
+    text: { type: String, required: true },
+    image: { type: String, required: false },
+  },
+  position: {
+    boundingRect: {
+      x1: { type: Number, required: true },
+      y1: { type: Number, required: true },
+      x2: { type: Number, required: true },
+      y2: { type: Number, required: true },
+      width: { type: Number, required: true },
+      height: { type: Number, required: true },
+      pageNumber: { type: Number, required: true },
+    },
+    rects: [
+      {
+        x1: { type: Number, required: true },
+        y1: { type: Number, required: true },
+        x2: { type: Number, required: true },
+        y2: { type: Number, required: true },
+        width: { type: Number, required: true },
+        height: { type: Number, required: true },
+        pageNumber: { type: Number, required: true },
+      },
+    ],
+    pageNumber: { type: Number, required: true },
+  },
+  comment: {
+    text: { type: String, required: true },
+    emoji: { type: String, required: false },
+  },
+});
+
+const Post = mongoose.model<PostDocument>('Post', PostSchema);
+
 // Define routes
 app.post('/posts', (req: Request, res: Response) => {
-  const postData: { title: string; content: Content } = req.body;
+  const postData: PostRequestData = req.body;
 
-  const newPost = new Post({
-    title: postData.title,
-    content: {
-      text: postData.content.text,
-      image: postData.content.image,
-    },
-  });
+  // Assuming there is only one key in postData
+  const url = Object.keys(postData)[0];
+  const postContent = postData[url];
 
-  newPost.save()
-    .then(result => {
-      res.json(result);
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Error saving post' });
+  if (url && postContent) {
+    const newPost = new Post({
+      title: url,
+      content: {
+        text: postContent.content.text || '',
+        image: postContent.content.image || '',
+      },
+      position: postContent.position || {},
+      comment: postContent.comment || {},
     });
+
+    newPost
+      .save()
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ error: 'Error saving post' });
+      });
+  } else {
+    res.status(400).json({ error: 'Invalid request format' });
+  }
 });
 
 // Start the server
